358904b5feca38c4cada021cadbaf81f240b3f60
Adrian Gierakowski <agierakowski@gmail.com>
2020-05-19 11:36:01 +0100

    add prepareAmountBN and preparePriceBN

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ module.exports = {
   Joi: require('./lib/Joi'),
   computeBuySellData: require('./lib/computeBuySellData'),
   prepareAmount: require('./lib/prepareAmount'),
+  prepareAmountBN: require('./lib/prepareAmountBN'),
   preparePrice: require('./lib/preparePrice'),
+  preparePriceBN: require('./lib/preparePriceBN'),
   splitSymbol: require('./lib/splitSymbol'),
   toBN: require('./lib/toBN')
 }
