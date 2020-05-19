38a08fc6673677c4a5e3a28ac71891690526a8f4
Adrian Gierakowski <agierakowski@gmail.com>
2020-05-19 10:59:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  BN: require('./lib/BN'),
+  Joi: require('./lib/Joi'),
+  computeBuySellData: require('./lib/computeBuySellData'),
+  prepareAmount: require('./lib/prepareAmount'),
+  preparePrice: require('./lib/preparePrice'),
+  splitSymbol: require('./lib/splitSymbol'),
+  toBN: require('./lib/toBN')
+}
