584cf65bccc088179b863f44787b009d5e406c3e
Adrian Gierakowski <agierakowski@gmail.com>
2020-09-25 20:47:22 +0100

    fix lint issues

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@ module.exports = {
     'ETH:UST': 'ETH:USDT',
     'BTC:UST': 'BTC:USDT',
     'LINK:UST': 'LINK:USDT',
-    'tETHUSD': 'tETHUST',
-    'tBTCUSD': 'tBTCUST',
+    tETHUSD: 'tETHUST',
+    tBTCUSD: 'tBTCUST',
     'tLINK:USD': 'tLINK:UST'
   }
 }
