f83595c9e5a9a676b4d8e4309f14da19fd44eae4
thiagokroger <thiagokroger@gmail.com>
2020-10-14 17:13:06 -0300

    feat: add usdt mapping for comp, uni and yfi

--- config/config.js
+++ config/config.js
@@ -1,14 +1,23 @@
+/* eslint-disable quote-props */
 module.exports = {
   tokenMapping: {
     USDT: 'USD',
     USDC: 'UDC'
   },
   USDTMapping: {
+    // dvf to bfx
+    'tETHUSD': 'tETHUST',
+    'tBTCUSD': 'tBTCUST',
+    'tLINK:USD': 'tLINK:UST',
+    'tCOMP:USD': 'tCOMP:UST',
+    'tUNIUSD': 'tUNIUST',
+    'tYFIUSD': 'tYFIUST',
+    // bfx to dvf
     'ETH:UST': 'ETH:USDT',
     'BTC:UST': 'BTC:USDT',
     'LINK:UST': 'LINK:USDT',
-    tETHUSD: 'tETHUST',
-    tBTCUSD: 'tBTCUST',
-    'tLINK:USD': 'tLINK:UST'
+    'COMP:UST': 'COMP:USDT',
+    'UNI:UST': 'UNI:USDT',
+    'YFI:UST': 'YFI:USDT'
   }
 }
