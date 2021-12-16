cea1c5f93b9d3ecf79ae69c1e7ff6432d4c74bd7
Sam Beale <sjpbeale@gmail.com>
2021-12-16 15:32:26 +0200

    JUP-288: Update USDT Mapping (#50)
    * JUP-288: Update USDT Mapping
    
    * JUP-288: Update bfx/dvf symbol tests
    
    * JUP-288: Handle MNA-MANA bfxSymbol/dvfsymbol
    
    * JUP-288: Add MANA->MNA tokenMapping

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@
 module.exports = {
   tokenMapping: {
     USDT: 'USD',
-    USDC: 'UDC'
+    USDC: 'UDC',
+    MANA: 'MNA'
   },
   USDTMapping: {
     // dvf to bfx
@@ -16,6 +17,12 @@ module.exports = {
     'tUDCUSD': 'tUDCUST',
     'tETHUDC': 'tETHUST',
     'tBTCUDC': 'tBTCUST',
+    't1INCH:USD': 't1INCH:UST',
+    'tMATIC:USD': 'tMATIC:UST',
+    'tAAVE:USD': 'tAAVE:UST',
+    'tSPELL:USD': 'tSPELL:UST',
+    'tMANA:USD': 'tMNAUSD',
+    'tCRVUSD': 'tCRVUST',
     // bfx to dvf
     'ETH:UST': 'ETH:USDT',
     'BTC:UST': 'BTC:USDT',
@@ -24,6 +31,12 @@ module.exports = {
     'UNI:UST': 'UNI:USDT',
     'YFI:UST': 'YFI:USDT',
     'SUSHI:UST': 'SUSHI:USDT',
-    'UDC:UST': 'USDC:USDT'
+    'UDC:UST': 'USDC:USDT',
+    '1INCH:UST': '1INCH:USDT',
+    'MATIC:UST': 'MATIC:USDT',
+    'AAVE:UST': 'AAVE:USDT',
+    'SPELL:UST': 'SPELL:USDT',
+    'MNA:USDT': 'MANA:USDT',
+    'CRV:UST': 'CRV:USDT'
   }
 }
