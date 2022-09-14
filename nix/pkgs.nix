bf802ccd6fd4758e06987dca491f8ed761d9e370
Adrian Gierakowski <agierakowski@gmail.com>
2022-09-14 15:56:06 +0100

    [JUP-419]: publish to npm registry at npmjs.com

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -4,4 +4,20 @@
   system ? builtins.currentSystem,
   overlays ? []
 }:
-  import sources.nixpkgs { inherit config system overlays; }
+let
+  allOverlays =
+    # These overlays augment centrally defined packages with things specific
+    # to this service.
+    [
+    ]
+    ++
+    overlays
+  ;
+
+  # This can be used to work against local version of copy of launch-deversifi
+  # repo instead of specific git commit defined in sources.json
+  # pkgsBasePath = ../../launch-deversifi;
+  pkgsBasePath = sources.launch-deversifi;
+  pkgsPath = pkgsBasePath + "/nix/pkgs.nix";
+in
+  import pkgsPath { inherit config system; overlays = allOverlays; }
